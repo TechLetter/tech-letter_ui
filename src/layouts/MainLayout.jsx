2e662e22b1a8cd349f02de8936374aa49ec463e8
dhkimxx <dhkimxx@naver.com>
2025-09-27 23:39:01 +0900

    Refactor: Layout 컴포넌트화

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,31 @@
-// layouts/MainLayout.jsx
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-      <Outlet />
+    <div className="w-[420px] mx-auto bg-gray-50 min-h-screen relative">
+      {/* 헤더 */}
+      <header className="fixed top-0 w-[420px] h-[50px] bg-white flex items-center justify-between px-4 shadow z-50">
+        <div className="text-[20px] font-bold font-sans">Tech Letter</div>
+        {/* <div className="flex items-center gap-3">
+          <div className="relative">
+            <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
+            <div className="absolute top-0 right-0 w-3 h-3 bg-red-500 rounded-full border-2 border-white"></div>
+          </div>
+          <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+        </div> */}
+      </header>
+
+      {/* 메인 컨텐츠 */}
+      <main className="pt-[65px] pb-[100px] px-4">
+        <Outlet />
+      </main>
+
+      {/* 푸터 */}
+      {/* <footer className="fixed bottom-0 w-[420px] h-[30px] bg-white flex justify-around items-center border-t border-gray-300 z-50">
+        <div className="w-[10px] h-[10px] bg-gray-300 rounded-full"></div>
+        <div className="w-[10px] h-[10px] bg-gray-300 rounded-full"></div>
+        <div className="w-[10px] h-[10px] bg-gray-300 rounded-full"></div>
+      </footer> */}
     </div>
-  )
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,47 +47,25 @@ export default function Home() {
   }, [hasMore, loading]);
 
   return (
-    <div className="w-[420px] mx-auto bg-gray-50 min-h-screen relative">
-      {/* 헤더 */}
-      <header className="fixed top-0 left-1/2 -translate-x-1/2 w-[420px] h-[84px] bg-white flex items-center justify-between px-4 shadow">
-        <div className="text-2xl font-bold font-sans">Tech Feed</div>
-        <div className="flex items-center gap-3">
-          <div className="relative">
-            <div className="w-6 h-6 bg-gray-300 rounded-full"></div>
-            <div className="absolute top-0 right-0 w-3 h-3 bg-red-500 rounded-full border-2 border-white"></div>
-          </div>
-          <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
-        </div>
-      </header>
+    <div className="flex flex-col items-center gap-[16px]">
+      {posts.map((post, index) => (
+        <PostCard
+          key={`${post.id}-${index}`}
+          blogName={post.blog_name}
+          postTitle={post.title}
+          postSummary={post.summary}
+          postTags={post.tags}
+          postThumbnailUrl={post.thumbnail_url}
+          postUrl={post.link}
+          postPublishedAt={post.published_at}
+          postViewCount={post.view_count}
+        />
+      ))}
 
-      {/* 포스트 리스트 */}
-      <main className="pt-[100px] pb-[100px] px-4 flex flex-col gap-[12px]">
-        {posts.map((post, index) => (
-          <PostCard
-            key={`${post.id}-${index}`}
-            blogName={post.blog_name}
-            postTitle={post.title}
-            postSummary={post.summary}
-            postTags={post.tags}
-            postThumbnailUrl={post.thumbnail_url}
-            postUrl={post.link}
-            postPublishedAt={post.published_at}
-            postViewCount={post.view_count}
-          />
-        ))}
-
-        {loading && <div className="text-center my-4">Loading...</div>}
-        {!hasMore && (
-          <div className="text-center my-4 text-gray-500">No more posts</div>
-        )}
-      </main>
-
-      {/* 푸터 */}
-      <footer className="fixed bottom-0 left-1/2 -translate-x-1/2 w-[420px] h-[84px] bg-white flex justify-around items-center border-t border-gray-300">
-        <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
-        <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
-        <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
-      </footer>
+      {loading && <div className="text-center my-4">Loading...</div>}
+      {!hasMore && (
+        <div className="text-center my-4 text-gray-500">No more posts</div>
+      )}
     </div>
   );
 }
