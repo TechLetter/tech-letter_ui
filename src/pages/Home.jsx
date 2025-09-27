e7856f3ffa78275b6c6739e3530cb95424d3d21a
dhkimxx <dhkimxx@naver.com>
2025-09-27 19:45:01 +0900

    Feat: TailwindCSS 설정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <h2>Home Page</h2>
-  }
+  return (
+    <h1 className="text-3xl font-bold text-blue-500 underline">
+      Hello world!
+    </h1>
+  )
+}
